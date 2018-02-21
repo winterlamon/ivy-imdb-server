54398e19acd5671ca1a1e7745778ecad156cdb67
github username <winter.lamon@gmail.com>
2018-02-21 11:59:27 -0500

    created people table

--- db/migrate/20180221073701_create_people.rb
+++ db/migrate/20180221073701_create_people.rb
@@ -1,7 +1,14 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
   def change
     create_table :people do |t|
-
+      t.string :name
+      t.string :birthday
+      t.string :photoUrl
+      t.string :profileUrl
+      t.string :title
+      t.string :url
+      t.float :rating
+      t.string :director
       t.timestamps
     end
   end
