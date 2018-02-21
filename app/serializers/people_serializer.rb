a93f03613fd7f5a57c292ca8ebc638055691e282
github username <winter.lamon@gmail.com>
2018-02-21 12:08:06 -0500

    customized output in people serializer

--- app/serializers/people_serializer.rb
+++ app/serializers/people_serializer.rb
@@ -1,3 +1,10 @@
 class PeopleSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :name, :photoUrl, :profileUrl, :mostKnownWork
+
+  def mostKnownWork
+      {title: object.title,
+      url: object.url,
+      rating: object.rating,
+      director: object.director}
+  end
 end
