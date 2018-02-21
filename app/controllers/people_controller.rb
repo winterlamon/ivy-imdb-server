fdb43a8d2f30e90b44e77912be8f289a7962dd10
github username <winter.lamon@gmail.com>
2018-02-21 15:05:55 -0500

    updated people_controller to strip whitespace from scraped entries

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,6 @@
 class PeopleController < ApplicationController
+  require "open-uri"
+  require "nokogiri"
 
   def create
     #get actor results based on birthday
@@ -14,18 +16,18 @@ class PeopleController < ApplicationController
 
     parsed_content.css('.lister-item').each do |item|
       People.find_or_create_by(
-        name: item.css('.lister-item-header').css('a').inner_html,
+        name: item.css('.lister-item-header').css('a').inner_html.strip,
         birthday: @birthday,
         photoUrl: item.css('.lister-item-image').css('img').attribute('src').value,
         profileUrl: "http://www.imdb.com" + item.css('.lister-item-header').css('a').attribute('href').value,
-        title: parsed_content.css('.lister-item-content').css('p').first.css('a').inner_text,
+        title: parsed_content.css('.lister-item-content').css('p').first.css('a').inner_text.strip,
         url: "http://www.imdb.com" + parsed_content.css('.lister-item-content').css('p').first.css('a').attribute('href').value,
         rating: parsed_work_content.css('.ratingValue').css('span').first.inner_text.to_f,
-        director: parsed_work_content.css('.credit_summary_item').first.css('a').css('span').inner_text
+        director: parsed_work_content.css('.credit_summary_item').first.css('a').css('span').inner_text.strip
       )
     end
 
-    @people = People.all.select(birthday: @birthday)
+    @people = People.all.where(birthday: @birthday)
 
     render json: @people
 
