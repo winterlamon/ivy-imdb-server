d5d0e073793912c34e9560cf126a67db787268ff
github username <winter.lamon@gmail.com>
2018-02-22 14:09:39 -0500

    updated people create to replace rating with 0 if work has not yet been rated

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,16 +15,29 @@ class PeopleController < ApplicationController
       knownWorkDoc = open("http://www.imdb.com" + item.css('p').first.css('a').attribute('href').value)
       workContent = knownWorkDoc.read
       parsed_work_content = Nokogiri::HTML(workContent)
-      People.find_or_create_by(
-        name: item.css('.lister-item-header').css('a').inner_html.strip,
-        birthday: @birthday,
-        photoUrl: item.css('.lister-item-image').css('img').attribute('src').value,
-        profileUrl: "http://www.imdb.com" + item.css('.lister-item-header').css('a').attribute('href').value,
-        title: item.css('.lister-item-content').css('p').first.css('a').inner_text.strip,
-        url: "http://www.imdb.com" + item.css('.lister-item-content').css('p').css('a').attribute('href').value,
-        rating: parsed_work_content.css('.ratingValue').css('span').first.inner_text.to_f,
-        director: parsed_work_content.css('.credit_summary_item').first.css('a').css('span').inner_text.strip
-      )
+      if parsed_work_content.css('.ratingValue').css('span').first
+        People.find_or_create_by(
+          name: item.css('.lister-item-header').css('a').inner_html.strip,
+          birthday: @birthday,
+          photoUrl: item.css('.lister-item-image').css('img').attribute('src').value,
+          profileUrl: "http://www.imdb.com" + item.css('.lister-item-header').css('a').attribute('href').value,
+          title: item.css('.lister-item-content').css('p').first.css('a').inner_text.strip,
+          url: "http://www.imdb.com" + item.css('.lister-item-content').css('p').css('a').attribute('href').value,
+          rating: parsed_work_content.css('.ratingValue').css('span').first.inner_text.to_f,
+          director: parsed_work_content.css('.credit_summary_item').first.css('a').css('span').inner_text.strip
+        )
+      else
+        People.find_or_create_by(
+          name: item.css('.lister-item-header').css('a').inner_html.strip,
+          birthday: @birthday,
+          photoUrl: item.css('.lister-item-image').css('img').attribute('src').value,
+          profileUrl: "http://www.imdb.com" + item.css('.lister-item-header').css('a').attribute('href').value,
+          title: item.css('.lister-item-content').css('p').first.css('a').inner_text.strip,
+          url: "http://www.imdb.com" + item.css('.lister-item-content').css('p').css('a').attribute('href').value,
+          rating: 0.0,
+          director: parsed_work_content.css('.credit_summary_item').first.css('a').css('span').inner_text.strip
+        )
+      end
     end
 
     people = People.all.where(birthday: @birthday)
